f7986fe46d021dc673e0e3838c492a4effe78f76
Makah <mecca-131@hotmail.com>
2019-11-01 21:11:38 -0500

    trying

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,19 +1,19 @@
-let MYAPIKEY ="tbz114zIETMv2GysLCNE0JPkNkinaaND";
-document.addEventListener("DOMContentLoader", init);
+let MYAPIKEY = "tbz114zIETMv2GysLCNE0JPkNkinaaND";
+document.addEventListener("DOMContentLoaded", init);
 function init(){
 
   document.getElementById("btn-search").addEventListener("click" ,  ev => {
     ev.preventDefault();
-    let url='https://api.giphy.com/v1/gifs/search?api_key=$(MYAPIKEY)&limit=10&q=&rating=';
+    let url='https://api.giphy.com/v1/gifs/search?api_key=${MYAPIKEY}&limit=10&q=&rating=';
     let str = document.getElementById("search").value.trim();
     url= url.concat(str);
     console.log(url);
 
-    fetch(url)
+  fetch(url)
     .then( Response => Response.json())
     .then( content => {
-console.log ( content.data)
-console.log('META' , content.mata);
+console.log ( content.data);
+console.log('META' , content.meta);
     })
 .catch(err =>{
   console.error(err);
