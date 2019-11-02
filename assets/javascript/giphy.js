1f75025a1c277162f79e6b6f8a6cd75ae33335f4
Makah <mecca-131@hotmail.com>
2019-11-01 20:53:26 -0500

    comment

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,19 +1,28 @@
+let MYAPIKEY ="tbz114zIETMv2GysLCNE0JPkNkinaaND";
+document.addEventListener("DOMContentLoader", init);
+function init(){
+
+  document.getElementById("btn-search").addEventListener("click" ,  ev => {
+    ev.preventDefault();
+    let url='https://api.giphy.com/v1/gifs/search?api_key=$(MYAPIKEY)&limit=10&q=&rating=';
+    let str = document.getElementById("search").value.trim();
+    url= url.concat(str);
+    console.log(url);
+
+    fetch(url)
+    .then( Response => Response.json())
+    .then( content => {
+console.log ( content.data)
+console.log('META' , content.mata);
+    })
+.catch(err =>{
+  console.error(err);
+})
+    
+  });
 
 
 
-function makah(){
-// my api number plus the link giphy provided
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=tbz114zIETMv2GysLCNE0JPkNkinaaND&limit=10");
-xhr.done(function(result) 
-{ console.log("success got data", result); });
-// a loop for my images but it's not working
-let makahs= result.data
-for(i in makahs){
-
-$('.loop').append('')
-
-
-}
 
 
 }
